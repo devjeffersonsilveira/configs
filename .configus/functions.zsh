9acc87d4480ebd8869cf3508469933271b7f6596
Jefferson Silveira <devjeffersonsilveira@gmail.com>
2017-10-04 14:04:51 -0300

    rmrmrmrmrmrmrm

--- .configus/functions.zsh
+++ .configus/functions.zsh
@@ -2,6 +2,7 @@
 function dockerclear {
     docker stop $(docker ps -a -q)
     docker rmi -f $(docker images -f "dangling=true" -q)
+    docker rm $(docker ps --filter=status=exited --filter=status=created -q)
     docker volume rm -f  $(docker volume ls -qf dangling=true)
     docker volume prunes
 }
