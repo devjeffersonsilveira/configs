cde070bf25566968716a42229a6cb032b1f7a6fc
Jefferson Silveira <devjeffersonsilveira@gmail.com>
2018-01-02 09:50:44 -0200

    breakSession

--- .configus/functions.zsh
+++ .configus/functions.zsh
@@ -6,3 +6,25 @@ function dockerclear {
     docker volume rm -f  $(docker volume ls -qf dangling=true)
     docker volume prunes
 }
+
+function breakSession {
+    PWD=$(pwd)
+    WHERE=here
+    SESSION_ID=$(qdbus org.kde.yakuake /yakuake/sessions org.kde.yakuake.activeSessionId)
+
+    if [[ $1 == "-t" ]]; then
+        WHERE=there
+    fi
+    if [[ $WHERE == there ]]; then
+        TERMINAL_ID=$(qdbus org.kde.yakuake /yakuake/sessions org.kde.yakuake.activeTerminalId)
+        qdbus org.kde.yakuake /yakuake/sessions runCommandInTerminal ${TERMINAL_ID} "clear"
+        SESSION_ID=$(qdbus org.kde.yakuake /yakuake/sessions org.kde.yakuake.addSession)
+        TERMINAL_ID=$(qdbus org.kde.yakuake /yakuake/sessions org.kde.yakuake.terminalIdsForSessionId ${SESSION_ID})
+    fi
+    if [[ $WHERE == here ]]; then
+        qdbus org.kde.yakuake /yakuake/sessions runCommandInTerminal ${TERMINAL_ID} "clear"
+        TERMINAL_ID=$(qdbus org.kde.yakuake /yakuake/sessions org.kde.yakuake.splitSessionTopBottom ${SESSION_ID})
+    fi
+
+    qdbus org.kde.yakuake /yakuake/sessions runCommandInTerminal ${TERMINAL_ID} "cd ${PWD}; clear"
+}
