c130579279f3b4bcc323d9add0f1798f070d2abc
Jefferson Silveira <devjeffersonsilveira@gmail.com>
2018-03-14 09:11:56 -0300

    Update functions.zsh

--- .configus/functions.zsh
+++ .configus/functions.zsh
@@ -1,10 +1,12 @@
 #!bin/bash
 function dockerclear {
-    docker stop $(docker ps -a -q)
-    docker rmi -f $(docker images -f "dangling=true" -q)
-    docker rm $(docker ps --filter=status=exited --filter=status=created -q)
-    docker volume rm -f  $(docker volume ls -qf dangling=true)
-    docker volume prunes
+    if [[ $1 == "-s" ]]; then
+        docker stop $(docker ps -a -q)
+        docker rm $(docker ps -a -q)
+    fi
+    
+    docker system prune -f
+    docker volume prune -f
 }
 
 function breakSession {
