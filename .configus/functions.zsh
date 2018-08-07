1db851b74d2ce8d2cc488fc6627fead751c623a3
Jefferson Silveira <devjeffersonsilveira@gmail.com>
2018-08-07 13:35:05 -0300

    update functions and alias

--- .configus/functions.zsh
+++ .configus/functions.zsh
@@ -1,15 +1,14 @@
-#!bin/bash
-function dockerclear {
+#!bin/zsh
+function dockerclear() {
     if [[ $1 == "-s" ]]; then
         docker stop $(docker ps -a -q)
         docker rm $(docker ps -a -q)
     fi
-    
-    docker system prune -f
-    docker volume prune -f
+
+    docker system prune -f --volumes
 }
 
-function breakSession {
+function breakSession() {
     PWD=$(pwd)
     WHERE=here
     SESSION_ID=$(qdbus org.kde.yakuake /yakuake/sessions org.kde.yakuake.activeSessionId)
@@ -29,4 +28,23 @@ function breakSession {
     fi
 
     qdbus org.kde.yakuake /yakuake/sessions runCommandInTerminal ${TERMINAL_ID} "cd ${PWD}; clear"
+}
+
+function proj() {
+    PROJECT=$@
+    cd ~/Projects
+
+    if [[ $1 == "simonetti" ]]; then
+        echo "vpn"
+        cd MoveisSimonetti/vpn
+        sudo nohup openvpn --config client.ovpn &
+        echo "catalogo"
+        cd ../catalogo
+        dck up -d app.catalogo.test
+        code ./
+        echo "webpdv"
+        cd ../webpdv
+        dck up -d webpdv
+        code ./
+    fi
 }
