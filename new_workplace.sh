61c47c7831df5f7c19f2e56aa912159cadaeb3a6
Jefferson Silveira <devjeffersonsilveira@gmail.com>
2019-04-24 19:45:52 -0300

    Update and rename new_workplace.md to new_workplace.sh

---
+++ new_workplace.sh
@@ -0,0 +1,34 @@
+#! bash
+
+## import configs from drive
+sudo chmod 0700 .ssh
+
+## permissions
+visudo
+
+## get default applications
+yaourt -S npm zsh fzf spotify --noconfirm
+
+## get develop applocations
+yay -S copyq php composer yarn nvm vim slack-desktop pip docker docker-compose visual-studio-code-bin postman --noconfirm
+
+## change default shell
+chsh -s /bin/zsh
+
+## add user to groups
+sudo gpasswd -a jalxes docker
+sudo gpasswd -a jalxes audio
+sudo gpasswd -a jalxes video
+
+## systemctl stuff
+sudo systemctl enable docker
+sudo systemctl enable gdm
+sudo systemctl disable lightdm
+sudo systemctl start gdm
+sudo systemctl stop lightdm
+
+## install pip stuff
+sudo pip install virtualenv
+
+## install composer global libs
+composer global require phpunit/phpunit:@stable friendsofphp/php-cs-fixer:@stable phpmd/phpmd:@stable squizlabs/php_codesniffer:@stable phpstan/phpstan:^0.9.2
