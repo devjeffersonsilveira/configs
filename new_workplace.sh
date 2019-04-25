91290ca32bc8f228e2b133a5f974db5c480963eb
Jefferson Silveira <devjeffersonsilveira@gmail.com>
2019-04-25 01:53:50 -0300

    real update, stages and stuff

--- new_workplace.sh
+++ new_workplace.sh
@@ -7,25 +7,30 @@ sudo chmod 0700 .ssh
 visudo
 
 ## get default applications
-yay -S npm zsh fzf spotify --noconfirm
-
-## get develop applocations
-yay -S copyq php composer yarn nvm vim slack-desktop pip docker docker-compose visual-studio-code-bin postman --noconfirm
+yay -S cronie cpupower baobab npm zsh fzf spotify --noconfirm
 
 ## change default shell
 chsh -s /bin/zsh
 
 ## add user to groups
-sudo gpasswd -a jalxes docker
 sudo gpasswd -a jalxes audio
 sudo gpasswd -a jalxes video
 
+## add .zshrc
+curl https://raw.githubusercontent.com/jalxes/configs/master/.zshrc -o ~/.zshrc
+
+
+
+## develop zone 
+
+## get develop applocations
+yay -S copyq php composer yarn nvm vim slack-desktop pip docker docker-compose visual-studio-code-bin postman --noconfirm
+
+## add user to groups
+sudo gpasswd -a jalxes docker
+
 ## systemctl stuff
 sudo systemctl enable docker
-sudo systemctl enable gdm
-sudo systemctl disable lightdm
-sudo systemctl start gdm
-sudo systemctl stop lightdm
 
 ## install pip stuff
 sudo pip install virtualenv
@@ -33,5 +38,45 @@ sudo pip install virtualenv
 ## install composer global libs
 composer global require phpunit/phpunit:@stable friendsofphp/php-cs-fixer:@stable phpmd/phpmd:@stable squizlabs/php_codesniffer:@stable phpstan/phpstan:^0.9.2
 
-## add ;zshrc
-curl https://raw.githubusercontent.com/jalxes/configs/master/.zshrc -o ~/.zshrc
+
+
+
+## Audio stuff
+
+## infra
+yay -S jack2-dbus
+yay -S linux-rt-lts jack2-dbus lib32-jack2 a2jmidid realtime-privileges ttymidi
+
+## more groups
+sudo gpasswd -a jalxes uucp
+sudo gpasswd -a jalxes lock
+sudo gpasswd -a jalxes realtime
+
+## locked memory
+ulimit -l unlimited
+
+## management apps
+yay -S cadence qtractor ardour-git carla audacity patchage jalv
+
+## samplers
+yay -S fluidsynth linuxsampler qsynth qsampler
+
+## helpers
+sherlock.lv2 qmidiarp
+
+## loopers
+yay -S sooperlooper-git freewheeling
+
+## base apps for playing 
+yay -S hydrogen-git hydrogen-drumkits 
+yay -S drmr-git zita-at1 guitarix2 non-daw
+
+## learning 
+yay -S musescore pianobooster
+
+## plugins packs
+yay -S calf vee-one x42-plugins distrho-vst-git 
+yay -S ArtyFX 
+
+## synths 
+yay -S aeolus setbfree zynaddsubfx yoshimi mda.lv2 helm bristol
